f57cc936fd794950010c5418fa7350e2bc17f0c1
bisnin0 <dustmq299@gmail.com>
2020-07-15 18:22:00 +0900

    first commit

---
+++ PrintTest.java
@@ -0,0 +1,9 @@
+public class PrintfTest 
+{
+	public static void main(String[] args) 
+	{
+		//
+		Strinf msg = "두번째 자바공부중..."
+		System.out.println(msg);
+	}
+}
---
+++ Test.java
@@ -0,0 +1,17 @@
+class Test
+{
+	static public void main(String[] args) 
+	{
+	byte b =10;
+		char ch = 'A';
+		int i = 100;
+		long l = 1000L;
+
+		//b = (byte)i;
+		// ch = (char)b;
+		//short a = (short)	ch;
+		//float f = l;
+		i = ch;
+		System.out.println(i);
+	}
+}
